feb453156699df89243c1fce1643ec1025fb58fd
Abhishek Sonwal <abhisonwal05@gmail.com>
2025-05-06 14:47:05 +0000

    eks version chnage

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ module "eks" {
   version = "20.8.3"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.29"
+  cluster_version = "1.32"
   subnet_ids      = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
 
